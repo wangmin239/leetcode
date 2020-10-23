a502320dab80871f58942b1412434f10fb1a7dda
Min Wang <wangmin@phytium.com.cn>
2020-10-23 16:22:22 +0800

    completely implement the program.

--- tree/669_trim_a_binary_search_tree.cpp
+++ tree/669_trim_a_binary_search_tree.cpp
@@ -15,5 +15,9 @@ public:
         if (root == nullptr)  return root;
         if (root->val < low)  return trimBST(root->right, low, high);
 		if (root->val > high) return trimBST(root->left, low, high);
+		
+		root->left = trimBST(root->left, low, high);
+		root->right = trimBst(root->right, low, high);
+		return root;
     }
 };
