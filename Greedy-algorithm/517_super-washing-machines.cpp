09ccfd57dad010cd1de9134f85ed48116215dff1
Min Wang <wangmin@phytium.com.cn>
2021-09-29 08:59:19 +0800

    [Greedy-algorithm]517_super-washing-machines

--- Greedy-algorithm/517_super-washing-machines.cpp
+++ Greedy-algorithm/517_super-washing-machines.cpp
@@ -16,3 +16,57 @@ public:
         return ans;
     }
 };
+
+
+
+
+class Solution {
+public:
+    int findMinMoves(vector<int>& machines) {
+        int i, size = machines.size(), sum = getSum(machines), average = sum / size;
+        if (average * size != sum) {
+            return -1;
+        }
+
+        int left = machines[0], wantedLeft = average, right, wantedRight = average * (size - 2);
+        int minMoves = max(abs(machines[0] - average), abs(machines[size - 1] - average));
+
+        for (i = 1; i < size - 1; ++i) {
+            int num = machines[i];
+            right = sum - num - left;
+
+            if (num >= average) {
+                int moves = num - average;
+                if (left > wantedLeft) {
+                    moves += (left - wantedLeft);
+                }
+                if (right > wantedRight) {
+                    moves += (right - wantedRight);
+                }
+                minMoves = max(minMoves, moves);
+
+            }
+
+            left += num;
+            wantedLeft += average;
+            wantedRight -= average;
+        }
+
+        return minMoves;
+    }
+
+    int getSum(vector<int>& machines) {
+        int sum = 0;
+
+        for (int num : machines) {
+            sum += num;
+        }
+
+        return sum;
+    }
+};
+
+作者：wen-rou-yi-dao-123
+链接：https://leetcode-cn.com/problems/super-washing-machines/solution/duo-tu-xiang-xi-jie-shi-yi-xia-yi-fu-de-rq3g4/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
