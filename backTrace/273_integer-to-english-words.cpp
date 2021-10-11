4e788cd0edb29ee7099864c5c62eac49ff1e2a43
Min Wang <wangmin@phytium.com.cn>
2021-10-11 09:55:31 +0800

    [backTrace]_integer-to-english-words

---
+++ backTrace/273_integer-to-english-words.cpp
@@ -0,0 +1,92 @@
+class Solution {
+public:
+    vector<string> singles = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    vector<string> teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    vector<string> tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    vector<string> thousands = {"", "Thousand", "Million", "Billion"};
+
+    string numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        string sb;
+        for (int i = 3, unit = 1000000000; i >= 0; i--, unit /= 1000) {
+            int curNum = num / unit;
+            if (curNum != 0) {
+                num -= curNum * unit;
+                string curr;
+                recursion(curr, curNum);
+                curr = curr + thousands[i] + " ";
+                sb = sb + curr;
+            }
+        }
+        while (sb.back() == ' ') {
+            sb.pop_back();
+        }
+        return sb;
+    }
+
+    void recursion(string & curr, int num) {
+        if (num == 0) {
+            return;
+        } else if (num < 10) {
+            curr = curr + singles[num] + " ";
+        } else if (num < 20) {
+            curr = curr + teens[num - 10] + " ";
+        } else if (num < 100) {
+            curr = curr + tens[num / 10] + " ";
+            recursion(curr, num % 10);
+        } else {
+            curr = curr + singles[num / 100] + " Hundred ";
+            recursion(curr, num % 100);
+        }
+    }
+};
+
+
+
+class Solution {
+public:
+    vector<string> singles = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    vector<string> teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    vector<string> tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    vector<string> thousands = {"", "Thousand", "Million", "Billion"};
+    
+    string numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        string sb;
+        for (int i = 3, unit = 1000000000; i >= 0; i--, unit /= 1000) {
+            int curNum = num / unit;
+            if (curNum != 0) {
+                num -= curNum * unit;
+                sb = sb + toEnglish(curNum) + thousands[i] + " ";
+            }
+        }
+        while (sb.back() == ' ') {
+            sb.pop_back();
+        }
+        return sb;
+    }
+
+    string toEnglish(int num) {
+        string curr;
+        int hundred = num / 100;
+        num %= 100;
+        if (hundred != 0) {
+            curr = curr + singles[hundred] + " Hundred ";
+        }
+        int ten = num / 10;
+        if (ten >= 2) {
+            curr = curr + tens[ten] + " ";
+            num %= 10;
+        }
+        if (num > 0 && num < 10) {
+            curr = curr + singles[num] + " ";
+        } else if (num >= 10) {
+            curr = curr + teens[num - 10] + " ";
+        }
+        return curr;
+    }
+};
