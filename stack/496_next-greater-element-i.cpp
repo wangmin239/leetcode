80da10a279a05e798f8f82886c50cc0b56025ad7
Min Wang <wangmin@phytium.com.cn>
2021-10-26 08:54:12 +0800

    [stack]496_next-greater-element-i

---
+++ stack/496_next-greater-element-i.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<int> res(m);
+        for (int i = 0; i < m; ++i) {
+            int j = 0;
+            while (j < n && nums2[j] != nums1[i]) {
+                ++j;
+            }
+            int k = j + 1;
+            while (k < n && nums2[k] < nums2[j]) {
+                ++k;
+            }
+            res[i] = k < n ? nums2[k] : -1;
+        }
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> hashmap;
+        stack<int> st;
+        for (int i = nums2.size() - 1; i >= 0; --i) {
+            int num = nums2[i];
+            while (!st.empty() && num >= st.top()) {
+                st.pop();
+            }
+            hashmap[num] = st.empty() ? -1 : st.top();
+            st.push(num);
+        }
+        vector<int> res(nums1.size());
+        for (int i = 0; i < nums1.size(); ++i) {
+            res[i] = hashmap[nums1[i]];
+        }
+        return res;
+    }
+};
