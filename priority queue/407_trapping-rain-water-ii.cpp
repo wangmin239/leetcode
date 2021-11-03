681e7556e77a72b95603cb8dc1ff533680a42861
Min Wang <wangmin@phytium.com.cn>
2021-11-03 19:36:21 +0800

    [priority queue]407_trapping-rain-water-ii

---
+++ priority queue/407_trapping-rain-water-ii.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        int m = heightMap.size(), n = heightMap[0].size();
+        int maxHeight = 0;
+        int dirs[] = {-1, 0, 1, 0, -1};
+
+        for (int i = 0; i < m; ++i) {
+            maxHeight = max(maxHeight, *max_element(heightMap[i].begin(), heightMap[i].end()));
+        }
+        vector<vector<int>> water(m, vector<int>(n, maxHeight));
+        queue<pair<int,int>> qu;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    if (water[i][j] > heightMap[i][j]) {
+                        water[i][j] = heightMap[i][j];
+                        qu.push(make_pair(i, j));
+                    }
+                }
+            }
+        }        
+        while (!qu.empty()) {
+            int x = qu.front().first, y = qu.front().second;
+            qu.pop();
+            for (int i = 0; i < 4; ++i) {
+                int nx = x + dirs[i], ny = y + dirs[i + 1];
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
+                    continue;
+                }
+                if (water[x][y] < water[nx][ny] && water[nx][ny] > heightMap[nx][ny]) {
+                    water[nx][ny] = max(water[x][y], heightMap[nx][ny]);
+                    qu.push(make_pair(nx, ny));
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                res += water[i][j] - heightMap[i][j];
+            }
+        }
+        return res;
+    }
+};
+
+
+typedef pair<int,int> pii;
+
+class Solution {
+public:
+    int trapRainWater(vector<vector<int>>& heightMap) {  
+        if (heightMap.size() <= 2 || heightMap[0].size() <= 2) {
+            return 0;
+        }  
+        int m = heightMap.size();
+        int n = heightMap[0].size();
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        vector<vector<bool>> visit(m, vector<bool>(n, false));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    pq.push({heightMap[i][j], i * n + j});
+                    visit[i][j] = true;
+                }
+            }
+        }
+
+        int res = 0;
+        int dirs[] = {-1, 0, 1, 0, -1};
+        while (!pq.empty()) {
+            pii curr = pq.top();
+            pq.pop();            
+            for (int k = 0; k < 4; ++k) {
+                int nx = curr.second / n + dirs[k];
+                int ny = curr.second % n + dirs[k + 1];
+                if( nx >= 0 && nx < m && ny >= 0 && ny < n && !visit[nx][ny]) {
+                    if (heightMap[nx][ny] < curr.first) {
+                        res += curr.first - heightMap[nx][ny]; 
+                    }
+                    visit[nx][ny] = true;
+                    pq.push({max(heightMap[nx][ny], curr.first), nx * n + ny});
+                }
+            }
+        }
+        
+        return res;
+    }
+};
