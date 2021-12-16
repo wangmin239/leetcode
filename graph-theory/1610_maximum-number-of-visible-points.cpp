cde154f88265d6646e0ee3934a3f2016cb01c017
Min Wang <wangmin@phytium.com.cn>
2021-12-16 10:08:59 +0800

    [graph-theory]1610_maximum-number-of-visible-points

---
+++ graph-theory/1610_maximum-number-of-visible-points.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int visiblePoints(vector<vector<int>>& points, int angle, vector<int>& location) {
+        int sameCnt = 0;
+        vector<double> polarDegrees;
+        for (auto & point : points) {
+            if (point[0] == location[0] && point[1] == location[1]) {
+                sameCnt++;
+                continue;
+            }
+            double degree = atan2(point[1] - location[1], point[0] - location[0]);
+            polarDegrees.emplace_back(degree);
+        }
+        sort(polarDegrees.begin(), polarDegrees.end());
+
+        int m = polarDegrees.size();
+        for (int i = 0; i < m; ++i) {
+            polarDegrees.emplace_back(polarDegrees[i] + 2 * M_PI);
+        }
+
+        int maxCnt = 0; 
+        double degree = angle * M_PI / 180;
+        for (int i = 0; i < m; ++i) {
+            auto it = upper_bound(polarDegrees.begin() + i, polarDegrees.end(), polarDegrees[i] + degree);
+            int curr = it - polarDegrees.begin() - i;
+            maxCnt = max(maxCnt, curr);
+        }
+        return maxCnt + sameCnt;
+    }
+};
+
+
+class Solution {
+public:
+    int visiblePoints(vector<vector<int>>& points, int angle, vector<int>& location) {
+        int sameCnt = 0;
+        vector<double> polarDegrees;
+        for (auto & point : points) {
+            if (point[0] == location[0] && point[1] == location[1]) {
+                sameCnt++;
+                continue;
+            }
+            double degree = atan2(point[1] - location[1], point[0] - location[0]);
+            polarDegrees.emplace_back(degree);
+        }
+        sort(polarDegrees.begin(), polarDegrees.end());
+
+        int m = polarDegrees.size();
+        for (int i = 0; i < m; ++i) {
+            polarDegrees.emplace_back(polarDegrees[i] + 2 * M_PI);
+        }
+
+        int maxCnt = 0;
+        int right = 0;
+        double degree = angle * M_PI / 180;
+        for (int i = 0; i < m; ++i) {
+            while (right < polarDegrees.size() && polarDegrees[right] <= polarDegrees[i] + degree) {
+                right++;
+            }
+            maxCnt = max(maxCnt, right - i);
+        }
+        return maxCnt + sameCnt;
+    }
+};
