c814506fb3ee37704501b0f29fb1140f40762337
Min Wang <wangmin@phytium.com.cn>
2022-01-29 09:07:15 +0800

    [BFS]1765_map-of-highest-peak

--- BFS/1765_map-of-highest-peak.cpp
+++ BFS/1765_map-of-highest-peak.cpp
@@ -22,9 +22,37 @@ public:
                     ans[x][y] = ans[p.first][p.second] + 1;
                     q.emplace(x, y);
                 }
-            }
+            }s
             q.pop();
         }
         return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int n = isWater.size();
+        int m = isWater[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 1e9+7));
+        for(int i=0; i<n; i++) {            //从左上开始遍历
+            for(int j=0; j<m; j++) {
+                if(isWater[i][j]) dp[i][j] = 0;
+                else {
+                    if(i > 0) dp[i][j] = min(dp[i][j], dp[i-1][j]+1);
+                    if(j > 0) dp[i][j] = min(dp[i][j], dp[i][j-1]+1);
+                }
+            }
+        }
+        for(int i=n-1; i>=0; i--) {         //从右下开始遍历
+            for(int j=m-1; j>=0; j--) {
+                if(isWater[i][j]) dp[i][j] = 0;
+                else {
+                    if(i < n-1) dp[i][j] = min(dp[i][j], dp[i+1][j]+1);
+                    if(j < m-1) dp[i][j] = min(dp[i][j], dp[i][j+1]+1);
+                }
+            }
+        }
+        return dp;
     }
 };
