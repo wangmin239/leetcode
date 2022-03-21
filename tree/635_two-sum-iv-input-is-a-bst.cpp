bb970045bab3d5a73809b4d03dff946d96bcd43d
Min Wang <wangmin@phytium.com.cn>
2022-03-21 09:35:57 +0800

    [tree]635_two-sum-iv-input-is-a-bst

---
+++ tree/635_two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,201 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        unordered_map<int, int> nums;
+        stack<TreeNode*> st;
+        
+        while (root != nullptr || st.empty() == false) {
+            if (root != nullptr) {
+                st.push(root);
+                root = root->left;
+            } else {
+                root = st.top();
+                st.pop();
+                auto [val, left, right] = *root;
+                ++nums[val];
+                root = right;
+            }
+        }
+        
+        for (auto [key, cnt] : nums) {
+            if (k == 2 * key) {
+                continue;
+            }
+            
+            if (nums.count(k - key) > 0) {
+                return true;
+            }             
+        }
+        
+        return false;
+    }
+};
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_set<int> hashTable;
+
+    bool findTarget(TreeNode *root, int k) {
+        if (root == nullptr) {
+            return false;
+        }
+        if (hashTable.count(k - root->val)) {
+            return true;
+        }
+        hashTable.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+};
+
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode *root, int k) {
+        unordered_set<int> hashTable;
+        queue<TreeNode *> que;
+        que.push(root);
+        while (!que.empty()) {
+            TreeNode *node = que.front();
+            que.pop();
+            if (hashTable.count(k - node->val)) {
+                return true;
+            }
+            hashTable.insert(node->val);
+            if (node->left != nullptr) {
+                que.push(node->left);
+            }
+            if (node->right != nullptr) {
+                que.push(node->right);
+            }
+        }
+        return false;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    vector<int> vec;
+
+    void inorderTraversal(TreeNode *node) {
+        if (node == nullptr) {
+            return;
+        }
+        inorderTraversal(node->left);
+        vec.push_back(node->val);
+        inorderTraversal(node->right);
+    }
+
+    bool findTarget(TreeNode *root, int k) {
+        inorderTraversal(root);
+        int left = 0, right = vec.size() - 1;
+        while (left < right) {
+            if (vec[left] + vec[right] == k) {
+                return true;
+            }
+            if (vec[left] + vec[right] < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return false;
+    }
+};
+
+
+class Solution {
+public:
+    TreeNode *getLeft(stack<TreeNode *> &stk) {
+        TreeNode *root = stk.top();
+        stk.pop();
+        TreeNode *node = root->right;
+        while (node != nullptr) {
+            stk.push(node);
+            node = node->left;
+        }
+        return root;
+    }
+
+    TreeNode *getRight(stack<TreeNode *> &stk) {
+        TreeNode *root = stk.top();
+        stk.pop();
+        TreeNode *node = root->left;
+        while (node != nullptr) {
+            stk.push(node);
+            node = node->right;
+        }
+        return root;
+    }
+
+    bool findTarget(TreeNode *root, int k) {
+        TreeNode *left = root, *right = root;
+        stack<TreeNode *> leftStack, rightStack;
+        leftStack.push(left);
+        while (left->left != nullptr) {
+            leftStack.push(left->left);
+            left = left->left;
+        }
+        rightStack.push(right);
+        while (right->right != nullptr) {
+            rightStack.push(right->right);
+            right = right->right;
+        }
+        while (left != right) {
+            if (left->val + right->val == k) {
+                return true;
+            }
+            if (left->val + right->val < k) {
+                left = getLeft(leftStack);
+            } else {
+                right = getRight(rightStack);
+            }
+        }
+        return false;
+    }
+};
+
