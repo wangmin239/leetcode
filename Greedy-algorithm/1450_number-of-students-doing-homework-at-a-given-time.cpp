b0c9c1aab6530383d7063cc0e487d30eca27407b
Min Wang <wangmin@phytium.com.cn>
2022-08-19 09:22:19 +0800

    [Greedy-algorithm]1450_number-of-students-doing-homework-at-a-given-time

--- Greedy-algorithm/1450_number-of-students-doing-homework-at-a-given-time.cpp
+++ Greedy-algorithm/1450_number-of-students-doing-homework-at-a-given-time.cpp
@@ -1,4 +1,40 @@
 class Solution {
+public: 
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        sort(startTime.begin(), startTime.end());
+        sort(endTime.begin(), endTime.end());
+        int lessStart = upper_bound(startTime.begin(), startTime.end(), queryTime) - startTime.begin();
+        int lessEnd = lower_bound(endTime.begin(), endTime.end(), queryTime) - endTime.begin();
+        return lessStart - lessEnd;
+    }
+};
+
+
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int n = startTime.size();
+        int maxEndTime = *max_element(endTime.begin(), endTime.end());
+        if (queryTime > maxEndTime) {
+            return 0;
+        }
+        vector<int> cnt(maxEndTime + 2);
+        for (int i = 0; i < n; i++) {
+            cnt[startTime[i]]++;
+            cnt[endTime[i] + 1]--;
+        }
+        int ans = 0;
+        for (int i = 0; i <= queryTime; i++) {
+            ans += cnt[i];
+        }
+        return ans;
+    }
+};
+
+
+
+
+class Solution {
 public:
     int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
         int sum = 0;
