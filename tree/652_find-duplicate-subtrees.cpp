b7b58cdc5d5b264e438ffafbab135bb67f8e98fe
Min Wang <wangmin@phytium.com.cn>
2022-09-06 10:06:26 +0800

    [tree]652_find-duplicate-subtrees

--- tree/652_find-duplicate-subtrees.cpp
+++ tree/652_find-duplicate-subtrees.cpp
@@ -1,30 +1,84 @@
-[ 1264.014666] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10754
-[ 1264.047998] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10454
-[ 1264.081329] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10554
-[ 1264.114660] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10654
-[ 1264.147990] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10754
-[ 1264.181322] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10454
-[ 1264.214653] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10554
-[ 1264.247984] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10654
-[ 1264.281315] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10754
-[ 1264.314647] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10454
-[ 1264.347986] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10554
-[ 1264.381308] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10654
-[ 1264.414640] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10754
-[ 1264.447971] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10454
-[ 1264.481302] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10554
-[ 1264.514632] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10654
-[ 1264.547964] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10754
-[ 1264.581297] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10454
-[ 1264.614626] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10554
-[ 1264.647959] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10654
-[ 1264.681290] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10754
-[ 1264.714621] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10454
-[ 1264.747952] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10554
-[ 1264.781287] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10654
-[ 1264.814618] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10754
-[ 1264.847949] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10454
-[ 1264.881278] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10554
-[ 1264.914608] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10654
-[ 1264.947941] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10754
-[ 1264.981270] phytium-jpeg 32b32000.jpeg: read interrupt status 0x10454
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        dfs(root);
+        return {repeat.begin(), repeat.end()};
+    }
+
+    int dfs(TreeNode* node) {
+        if (!node) {
+            return 0;
+        }
+        auto tri = tuple{node->val, dfs(node->left), dfs(node->right)};
+        if (auto it = seen.find(tri); it != seen.end()) {
+            repeat.insert(it->second.first);
+            return it->second.second;
+        }
+        else {
+            seen[tri] = {node, ++idx};
+            return idx;
+        }
+    }
+
+private:
+    static constexpr auto tri_hash = [fn = hash<int>()](const tuple<int, int, int>& o) -> size_t {
+        auto&& [x, y, z] = o;
+        return (fn(x) << 24) ^ (fn(y) << 8) ^ fn(z);
+    };
+
+    unordered_map<tuple<int, int, int>, pair<TreeNode*, int>, decltype(tri_hash)> seen{0, tri_hash};
+    unordered_set<TreeNode*> repeat;
+    int idx = 0;
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        dfs(root);
+        return {repeat.begin(), repeat.end()};
+    }
+
+    string dfs(TreeNode* node) {
+        if (!node) {
+            return "";
+        }
+        string serial = to_string(node->val) + "(" + dfs(node->left) + ")(" + dfs(node->right) + ")";
+        if (auto it = seen.find(serial); it != seen.end()) {
+            repeat.insert(it->second);
+        }
+        else {
+            seen[serial] = node;
+        }
+        return serial;
+    }
+
+private:
+    unordered_map<string, TreeNode*> seen;
+    unordered_set<TreeNode*> repeat;
+};
