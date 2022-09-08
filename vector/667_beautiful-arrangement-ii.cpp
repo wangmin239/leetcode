aee2405d56f60f6c5f5ab5325f877894df19ec11
Min Wang <wangmin@phytium.com.cn>
2022-09-08 09:16:45 +0800

    [vector]667_beautiful-arrangement-ii

---
+++ vector/667_beautiful-arrangement-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> constructArray(int n, int k) {
+        vector<int> answer;
+        for (int i = 1; i < n - k; ++i) {
+            answer.push_back(i);
+        }
+        for (int i = n - k, j = n; i <= j; ++i, --j) {
+            answer.push_back(i);
+            if (i != j) {
+                answer.push_back(j);
+            }
+        }
+        return answer;
+    }
+};
