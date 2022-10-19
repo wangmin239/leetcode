b1832a02d12e9e86edc4b4061604312905257d8c
Min Wang <wangmin@phytium.com.cn>
2022-10-19 09:28:31 +0800

    [vector]1700_number-of-students-unable-to-eat-lunch

---
+++ vector/1700_number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int num = students.size();
+        int left = 0;
+        int right = num - 1; 
+        sort(students.begin(), students.end(), greater<int>());
+        
+        for (int val : sandwiches) {
+            if (val == students[left]) {
+                left++;
+            } else if (val == students[right]) {
+                right--;
+            } else {
+                break;
+            }
+        }
+        return right - left + 1;
+    }
+};
+
+
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int s1 = accumulate(students.begin(), students.end(), 0);
+        int s0 = students.size() - s1;
+        for (int i = 0; i < sandwiches.size(); i++) {
+            if (sandwiches[i] == 0 && s0 > 0) {
+                s0--;
+            } else if (sandwiches[i] == 1 && s1 > 0) {
+                s1--;
+            } else {
+                break;
+            }
+        }
+        return s0 + s1;
+    }
+};
