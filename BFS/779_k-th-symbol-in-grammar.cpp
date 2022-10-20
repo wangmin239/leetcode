934b716e5a3d5e71fd4c91075933c9a2d6ca9d59
Min Wang <wangmin@phytium.com.cn>
2022-10-20 09:34:24 +0800

    [BFS]779_k-th-symbol-in-grammar

---
+++ BFS/779_k-th-symbol-in-grammar.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+        return (k & 1) ^ 1 ^ kthGrammar(n - 1, (k + 1) / 2);
+    }
+};
+
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if (k == 1) {
+            return 0;
+        }
+        if (k > (1 << (n - 2))) {
+            return 1 ^ kthGrammar(n - 1, k - (1 << (n - 2)));
+        }
+        return kthGrammar(n - 1, k);
+    }
+};
+
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        // return __builtin_popcount(k - 1) & 1;
+        k--;
+        int res = 0;
+        while (k > 0) {
+            k &= k - 1;
+            res ^= 1;
+        }
+        return res;
+    }
+};
+
