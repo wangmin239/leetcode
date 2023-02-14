b4d70bd25789490c65cc58af22f04387f6a0758a
Min Wang <wangmin@phytium.com.cn>
2023-02-14 12:40:18 +0800

    [Greedy-algorithm]1124_longest-well-performing-interva

---
+++ Greedy-algorithm/1124_longest-well-performing-interva.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int longestWPI(vector<int>& hours) {
+        int n = hours.size();
+        vector<int> s(n + 1);
+        stack<int> stk;
+        stk.push(0);
+        for (int i = 1; i <= n; i++) {
+            s[i] = s[i - 1] + (hours[i - 1] > 8 ? 1 : -1);
+            if (s[stk.top()] > s[i]) {
+                stk.push(i);
+            }
+        }
+
+        int res = 0;
+        for (int r = n; r >= 1; r--) {
+            while (stk.size() && s[stk.top()] < s[r]) {
+                res = max(res, r - stk.top());
+                stk.pop();
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int longestWPI(vector<int>& hours) {
+        int n = hours.size();
+        vector<int> preSum(n + 1, 0);
+        const int limit = 8;
+        for (int i = 0; i < n; i++) {
+            int val = hours[i] > limit ? 1 : -1;
+            preSum[i + 1] = preSum[i] + val;
+        }
+        int ans = 0;
+        for (int left = 0; left < n; left++) {
+            int right = n;
+            
+            while (right > left) {
+                if (preSum[right] - preSum[left] > 0) {
+                    break;
+                }
+                right--;
+            }
+            ans = max(ans, right - left);
+        }
+        
+        return ans;
+    }
+};
