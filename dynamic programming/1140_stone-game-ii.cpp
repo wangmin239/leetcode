36c75e425e60f84ed6e77aa62f163b997d9462bd
Min Wang <wangmin@phytium.com.cn>
2023-02-22 08:46:13 +0800

    [dynamic programming]1140_stone-game-ii

--- dynamic programming/1140_stone-game-ii.cpp
+++ dynamic programming/1140_stone-game-ii.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    int stoneGameII(vector<int>& piles) {
+        int len = piles.size();
+        int sum = 0; 
+        // dp[i][j]表示当前是第i波，m = j;
+        vector<vector<int>>dp(len + 1, vector<int>(len + 1, 0));
+        for(int i = len - 1; i >= 0; i--){
+            sum += piles[i];// 表示当前所剩下的所有棋子的和
+            for(int M = 1; M <= len; M++){
+                if(i + 2 * M >= len){
+                    dp[i][M] = sum;
+                    continue;
+                }
+                for(int x = 1; i + x <= len && x <= 2 * M; x++){
+                    dp[i][M] = max(dp[i][M], sum - dp[i + x][max(M, x)]);
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
+
+
+
+class Solution {
+public:
     int count=0;
     int max_count=0;
     vector<vector<int>> used;
@@ -37,3 +62,6 @@ public:
         return used[k][M];
     }
 };
+
+
+
