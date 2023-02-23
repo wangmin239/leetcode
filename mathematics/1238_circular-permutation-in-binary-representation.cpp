825498d696ddf7f9c6f838e9b96cc00dbb90d77a
Min Wang <wangmin@phytium.com.cn>
2023-02-23 08:53:20 +0800

    [mathematics]1238_circular-permutation-in-binary-representation

---
+++ mathematics/1238_circular-permutation-in-binary-representation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> circularPermutation(int n, int start) {
+        vector<int> ret(1 << n);
+        for (int i = 0; i < ret.size(); i++) {
+            ret[i] = (i >> 1) ^ i ^ start;
+        }
+        return ret;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> circularPermutation(int n, int start) {
+        vector<int> ret;
+        ret.reserve(1 << n);
+        ret.push_back(start);
+        for (int i = 1; i <= n; i++) {
+            int m = ret.size();
+            for (int j = m - 1; j >= 0; j--) {
+                ret.push_back(((ret[j] ^ start) | (1 << (i - 1))) ^ start);
+            }
+        }
+        return ret;
+    }
+};
