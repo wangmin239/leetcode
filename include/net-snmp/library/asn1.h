fe813b9cdfac0fe4935ed581cfa9718f86802db1
Min Wang <wangmin@phytium.com.cn>
2023-03-17 12:15:45 +0800

    [snmp] modify structure

--- include/net-snmp/library/asn1.h
+++ include/net-snmp/library/asn1.h
@@ -9,6 +9,11 @@
 #define IS_EXTENSION_ID(byte)  (((byte) & ASN_EXTENSION_ID) == ASN_EXTENSION_ID)
 
 struct counter64 {
-    unsigned long        high;
-    unsigned long        low;
+#if     __SIZEOF_POINTER__ == 4   
+        unsigned long        high;
+        unsigned long        low;
+#elif   __SIZEOF_POINTER__ == 8
+        unsigned int         high;
+        unsigned int         low;
+#endif
 };
