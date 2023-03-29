b5f9c322c290227ce2f3449a7b91c656e7ed2db3
Min Wang <wangmin@phytium.com.cn>
2023-03-29 09:22:39 +0800

    [dynamic programming]1641_count-sorted-vowel-strings

---
+++ dynamic programming/1641_count-sorted-vowel-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        vector<int> dp(5, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < 5; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return accumulate(dp.begin(), dp.end(), 0);
+    }
+};
+
+
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+    }
+};
