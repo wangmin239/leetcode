65b111f1627370db7d753425550029c5d27739fa
Min Wang <wangmin@phytium.com.cn>
2023-07-26 09:19:34 +0800

    [graph-theory]1326_minimum-number-of-taps-to-open-to-water-a-garden

---
+++ graph-theory/1326_minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        vector<int> rightMost(n + 1);
+        iota(rightMost.begin(), rightMost.end(), 0);
+        for (int i = 0; i <= n; i++) {
+            int start = max(0, i - ranges[i]);
+            int end = min(n, i + ranges[i]);
+            rightMost[start] = max(rightMost[start], end);
+        }
+        int last = 0, ret = 0, pre = 0;
+        for (int i = 0; i < n; i++) {
+            last = max(last, rightMost[i]);
+            if (i == last) {
+                return -1;
+            }
+            if (i == pre) {
+                ret++;
+                pre = last;
+            }
+        }
+        return ret;
+    }
+};
