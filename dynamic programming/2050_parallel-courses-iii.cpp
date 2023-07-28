3f74f1fa588727f2539df637c4b8d61e5993b9fe
Min Wang <wangmin@phytium.com.cn>
2023-07-28 09:51:43 +0800

    [dynamic programming]2050_parallel-courses-iii

--- dynamic programming/2050_parallel-courses-iii.cpp
+++ dynamic programming/2050_parallel-courses-iii.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        vector<vector<int>> grid(n);
+        vector<int> size(n);
+        for (auto&& x : relations) {
+            grid[x[0] - 1].emplace_back(x[1] - 1);
+            ++size[x[1] - 1];
+        }
+        queue<int> q;
+        for (int i = 0;i < n;++i) {
+            if (size[i] == 0) q.emplace(i);
+        }
+        vector<int> dp(n);
+        while (!q.empty()) {
+            int x = q.front(); q.pop();
+            dp[x] += time[x];
+            for (auto&& y : grid[x]) {
+                --size[y];
+                if (size[y] == 0) q.emplace(y);
+                dp[y] = max(dp[x], dp[y]);
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
+
+class Solution {
+public:
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
         int mx = 0;
         vector<vector<int>> prev(n + 1);
         for (auto &relation : relations) {
