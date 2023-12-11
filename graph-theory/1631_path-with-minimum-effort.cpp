1be6518c077516ae5c57ac60811cd2fefb910c04
Min Wang <wangmin@phytium.com.cn>
2023-12-11 19:19:32 +0800

    [graph-theory]1631_path-with-minimum-effort

---
+++ graph-theory/1631_path-with-minimum-effort.cpp
@@ -0,0 +1,162 @@
+class Solution {
+private:
+    static constexpr int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int m = heights.size();
+        int n = heights[0].size();
+        
+        auto tupleCmp = [](const auto& e1, const auto& e2) {
+            auto&& [x1, y1, d1] = e1;
+            auto&& [x2, y2, d2] = e2;
+            return d1 > d2;
+        };
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, decltype(tupleCmp)> q(tupleCmp);
+        q.emplace(0, 0, 0);
+
+        vector<int> dist(m * n, INT_MAX);
+        dist[0] = 0;
+        vector<int> seen(m * n);
+
+        while (!q.empty()) {
+            auto [x, y, d] = q.top();
+            q.pop();
+            int id = x * n + y;
+            if (seen[id]) {
+                continue;
+            }
+            if (x == m - 1 && y == n - 1) {
+                break;
+            }
+            seen[id] = 1;
+            for (int i = 0; i < 4; ++i) {
+                int nx = x + dirs[i][0];
+                int ny = y + dirs[i][1];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && max(d, abs(heights[x][y] - heights[nx][ny])) < dist[nx * n + ny]) {
+                    dist[nx * n + ny] = max(d, abs(heights[x][y] - heights[nx][ny]));
+                    q.emplace(nx, ny, dist[nx * n + ny]);
+                }
+            }
+        }
+        
+        return dist[m * n - 1];
+    }
+};
+
+
+// 并查集模板
+class UnionFind {
+public:
+    vector<int> parent;
+    vector<int> size;
+    int n;
+    // 当前连通分量数目
+    int setCount;
+    
+public:
+    UnionFind(int _n): n(_n), setCount(_n), parent(_n), size(_n, 1) {
+        iota(parent.begin(), parent.end(), 0);
+    }
+    
+    int findset(int x) {
+        return parent[x] == x ? x : parent[x] = findset(parent[x]);
+    }
+    
+    bool unite(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        if (x == y) {
+            return false;
+        }
+        if (size[x] < size[y]) {
+            swap(x, y);
+        }
+        parent[y] = x;
+        size[x] += size[y];
+        --setCount;
+        return true;
+    }
+    
+    bool connected(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        return x == y;
+    }
+};
+
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int m = heights.size();
+        int n = heights[0].size();
+        vector<tuple<int, int, int>> edges;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int id = i * n + j;
+                if (i > 0) {
+                    edges.emplace_back(id - n, id, abs(heights[i][j] - heights[i - 1][j]));
+                }
+                if (j > 0) {
+                    edges.emplace_back(id - 1, id, abs(heights[i][j] - heights[i][j - 1]));
+                }
+            }
+        }
+        sort(edges.begin(), edges.end(), [](const auto& e1, const auto& e2) {
+            auto&& [x1, y1, v1] = e1;
+            auto&& [x2, y2, v2] = e2;
+            return v1 < v2;
+        });
+
+        UnionFind uf(m * n);
+        int ans = 0;
+        for (const auto [x, y, v]: edges) {
+            uf.unite(x, y);
+            if (uf.connected(0, m * n - 1)) {
+                ans = v;
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
+
+class Solution {
+private:
+    static constexpr int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int m = heights.size();
+        int n = heights[0].size();
+        int left = 0, right = 999999, ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            queue<pair<int, int>> q;
+            q.emplace(0, 0);
+            vector<int> seen(m * n);
+            seen[0] = 1;
+            while (!q.empty()) {
+                auto [x, y] = q.front();
+                q.pop();
+                for (int i = 0; i < 4; ++i) {
+                    int nx = x + dirs[i][0];
+                    int ny = y + dirs[i][1];
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && !seen[nx * n + ny] && abs(heights[x][y] - heights[nx][ny]) <= mid) {
+                        q.emplace(nx, ny);
+                        seen[nx * n + ny] = 1;
+                    }
+                }
+            }
+            if (seen[m * n - 1]) {
+                ans = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
