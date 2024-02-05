767688ee19d237dd848f6f34b5d62ef55eb903e0
Min Wang <wangmin@phytium.com.cn>
2024-02-05 09:30:28 +0800

    [dynamic-programming]1696_jump-game-vi

---
+++ dynamic-programming/1696_jump-game-vi.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        deque<int> queue;
+        queue.push_back(0);
+        for (int i = 1; i < n; i++) {
+            while (!queue.empty() && queue.front() < i - k) {
+                queue.pop_front();
+            }
+            dp[i] = dp[queue.front()] + nums[i];
+            while (!queue.empty() && dp[queue.back()] <= dp[i]) {
+                queue.pop_back();
+            }
+            queue.push_back(i);
+        }
+        return dp[n - 1];
+    }
+};
