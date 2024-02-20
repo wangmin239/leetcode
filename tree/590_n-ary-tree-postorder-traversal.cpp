09238480c62717b3223ffdad6aca4b84ac62c496
Min Wang <wangmin@phytium.com.cn>
2024-02-20 08:50:47 +0800

    [tree]590_n-ary-tree-postorder-traversal

--- tree/590_n-ary-tree-postorder-traversal.cpp
+++ tree/590_n-ary-tree-postorder-traversal.cpp
@@ -17,6 +17,152 @@ public:
     }
 };
 */
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        if (root == nullptr) {
+            return res;
+        }
+
+        stack<Node *> st;
+        unordered_set<Node *> visited;
+        st.emplace(root);
+        while (!st.empty()) {
+            Node * node = st.top();
+            /* 如果当前节点为叶子节点或者当前节点的子节点已经遍历过 */
+            if (node->children.size() == 0 || visited.count(node)) {
+                res.emplace_back(node->val);
+                st.pop();
+                continue;
+            }
+            for (auto it = node->children.rbegin(); it != node->children.rend(); it++) {
+                st.emplace(*it);
+            }
+            visited.emplace(node);
+        }       
+        return res;
+    }
+};
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        if (root == nullptr) {
+            return res;
+        }
+
+        stack<Node *> st;
+        st.emplace(root);
+        while (!st.empty()) {
+            Node * node = st.top();
+            st.pop();
+            res.emplace_back(node->val);
+            for (auto &item : node->children) {
+                st.emplace(item);
+            }
+        }       
+        reverse(res.begin(), res.end()); 
+        return res;
+    }
+};
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        stack<Node*> st;
+
+        while (root != nullptr || st.empty() != true) {
+            if (root != nullptr) {
+                st.push(root);
+                if (root->children.size() > 0)
+                    root = root->children.front();
+                else
+                    root = nullptr;
+            } else {
+               auto node = st.top();
+               ans.push_back(node->val);
+               st.pop();
+
+               if (st.empty() == false) {
+                    auto parent = st.top();
+                    auto iter = parent->children.begin();
+                    auto end = parent->children.end();
+                    while (iter != end) {
+                        if (*iter == node) {
+                            ++iter;
+                            break;
+                        }
+                        ++iter;
+                    }
+                    root = (iter == end) ? nullptr : *iter;
+                }
+
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
 
 class Solution {
 public:
