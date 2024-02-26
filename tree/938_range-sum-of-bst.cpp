37a5b399d0bdcca8911cb2170bf2990a3ed05739
Min Wang <wangmin@phytium.com.cn>
2024-02-26 09:37:00 +0800

    [tree]938_range-sum-of-bst

---
+++ tree/938_range-sum-of-bst.cpp
@@ -0,0 +1,103 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode *root, int low, int high) {
+        int sum = 0;
+        queue<TreeNode*> q({root});
+        while (!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            if (node == nullptr) {
+                continue;
+            }
+            if (node->val > high) {
+                q.push(node->left);
+            } else if (node->val < low) {
+                q.push(node->right);
+            } else {
+                sum += node->val;
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        return sum;
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode *root, int low, int high) {
+        if (root == nullptr) {
+            return 0;
+        }
+        if (root->val > high) {
+            return rangeSumBST(root->left, low, high);
+        }
+        if (root->val < low) {
+            return rangeSumBST(root->right, low, high);
+        }
+        return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    }
+};
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        vector<int> vals;
+        stack<TreeNode*> st;
+        
+        while (root != nullptr || st.empty() != true) {
+            if (root != nullptr) {
+                st.push(root);
+                root = root->left;
+            } else {
+                auto node = st.top();
+                vals.push_back(node->val);
+                root = node->right;
+                st.pop();
+            }
+        }
+        
+        auto lowIter = lower_bound(vals.begin(), vals.end(), low);
+        auto upIter = upper_bound(vals.begin(), vals.end(), high);
+        
+        return accumulate(lowIter, upIter, 0);
+    }
+};
