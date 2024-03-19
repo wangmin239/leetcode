44e82f0d7b7bc59c4aa42473df0525a77baa8a70
Min Wang <wangmin@phytium.com.cn>
2024-03-19 09:00:00 +0800

    [Greedy-algorithm]1793_maximum-score-of-a-good-subarray

---
+++ Greedy-algorithm/1793_maximum-score-of-a-good-subarray.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = k - 1, right = k + 1;
+        int ans = 0;
+        for (int i = nums[k];; --i) {
+            while (left >= 0 && nums[left] >= i) {
+                --left;
+            }
+            while (right < n && nums[right] >= i) {
+                ++right;
+            }
+            ans = max(ans, (right - left - 1) * i);
+            if (left == -1 && right == n) {
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = k - 1, right = k + 1;
+        int ans = 0;
+        for (int i = nums[k];;) {
+            while (left >= 0 && nums[left] >= i) {
+                --left;
+            }
+            while (right < n && nums[right] >= i) {
+                ++right;
+            }
+            ans = max(ans, (right - left - 1) * i);
+            if (left == -1 && right == n) {
+                break;
+            }
+            i = max((left == -1 ? -1 : nums[left]), (right == n ? -1 : nums[right]));
+            if (i == -1) {
+                break;
+            }
+        }
+        return ans;
+    }
+};
