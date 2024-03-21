ead18d0f8984f1d079ecf63f5bc3fa60e8f5157c
Min Wang <wangmin@phytium.com.cn>
2024-03-21 10:41:46 +0800

    [mathematics]2671_frequency-tracker

--- mathematics/2671_frequency-tracker.cpp
+++ mathematics/2671_frequency-tracker.cpp
@@ -51,3 +51,45 @@ public:
  * obj->deleteOne(number);
  * bool param_3 = obj->hasFrequency(frequency);
  */
+ 
+ 
+ 
+ class FrequencyTracker {
+public:
+    FrequencyTracker():freq(N), freq_cnt(N) {
+        
+    }
+
+    void add(int number) {
+        --freq_cnt[freq[number]];
+        ++freq[number];
+        ++freq_cnt[freq[number]];
+    }
+    
+    void deleteOne(int number) {
+        if (freq[number] == 0) {
+            return;
+        }
+        --freq_cnt[freq[number]];
+        --freq[number];
+        ++freq_cnt[freq[number]];
+    }
+    
+    bool hasFrequency(int frequency) {
+        return freq_cnt[frequency];
+    }
+
+private:
+    static constexpr int N = 100001;
+    vector<int> freq;
+    vector<int> freq_cnt;
+};
+
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * FrequencyTracker* obj = new FrequencyTracker();
+ * obj->add(number);
+ * obj->deleteOne(number);
+ * bool param_3 = obj->hasFrequency(frequency);
+ */
+ 
