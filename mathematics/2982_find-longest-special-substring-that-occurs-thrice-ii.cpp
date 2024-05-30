0b89ffe27447cd59f03cb49d189999cb4ca0ad40
Min Wang <wangmin@phytium.com.cn>
2024-05-30 10:13:44 +0800

    [mathematics]2982_find-longest-special-substring-that-occurs-thrice-ii

--- mathematics/2982_find-longest-special-substring-that-occurs-thrice-ii.cpp
+++ mathematics/2982_find-longest-special-substring-that-occurs-thrice-ii.cpp
@@ -64,10 +64,7 @@ public:
     }
 };
 
-作者：力扣官方题解
-链接：https://leetcode.cn/problems/find-longest-special-substring-that-occurs-thrice-ii/solutions/2792836/zhao-chu-chu-xian-zhi-shao-san-ci-de-zui-pdem/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
 class Solution {
 public:
     int maximumLength(string s) {
