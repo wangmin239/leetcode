33df98a732a63c03ab2d0c25b1fbc41244c82c88
Min Wang <wangmin@phytium.com.cn>
2024-11-10 20:04:13 +0800

    [mathematics]540_single-element-in-a-sorted-array

---
+++ mathematics/540_single-element-in-a-sorted-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int ans = 0;
+        
+        for (int val : nums) {
+            ans ^= val;
+        }
+        
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (nums[mid] == nums[mid ^ 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+};
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            mid -= mid & 1;
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+};
