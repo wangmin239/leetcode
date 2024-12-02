63554a26ee31c513b11a14b48ae1041a3970d0a3
Min Wang <wangmin@phytium.com.cn>
2024-12-02 08:56:18 +0800

    [dynamic-programming]52_n-queens-ii

---
+++ dynamic-programming/52_n-queens-ii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        unordered_set<int> columns, diagonals1, diagonals2;
+        return backtrack(n, 0, columns, diagonals1, diagonals2);
+    }
+
+    int backtrack(int n, int row, unordered_set<int>& columns, unordered_set<int>& diagonals1, unordered_set<int>& diagonals2) {
+        if (row == n) {
+            return 1;
+        } else {
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (columns.find(i) != columns.end()) {
+                    continue;
+                }
+                int diagonal1 = row - i;
+                if (diagonals1.find(diagonal1) != diagonals1.end()) {
+                    continue;
+                }
+                int diagonal2 = row + i;
+                if (diagonals2.find(diagonal2) != diagonals2.end()) {
+                    continue;
+                }
+                columns.insert(i);
+                diagonals1.insert(diagonal1);
+                diagonals2.insert(diagonal2);
+                count += backtrack(n, row + 1, columns, diagonals1, diagonals2);
+                columns.erase(i);
+                diagonals1.erase(diagonal1);
+                diagonals2.erase(diagonal2);
+            }
+            return count;
+        }
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int totalNQueens(int n) {
+        return solve(n, 0, 0, 0, 0);
+    }
+
+    int solve(int n, int row, int columns, int diagonals1, int diagonals2) {
+        if (row == n) {
+            return 1;
+        } else {
+            int count = 0;
+            int availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2));
+            while (availablePositions != 0) {
+                int position = availablePositions & (-availablePositions);
+                availablePositions = availablePositions & (availablePositions - 1);
+                count += solve(n, row + 1, columns | position, (diagonals1 | position) << 1, (diagonals2 | position) >> 1);
+            }
+            return count;
+        }
+    }
+};
