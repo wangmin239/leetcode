856b5ec73152b70fdef6f91dc46968e2a2ab388b
Min Wang <wangmin@phytium.com.cn>
2025-01-17 09:10:06 +0800

    [mathematics]3095_shortest-subarray-with-or-at-least-k-i

--- mathematics/3095_shortest-subarray-with-or-at-least-k-i.cpp
+++ mathematics/3095_shortest-subarray-with-or-at-least-k-i.cpp
@@ -50,27 +50,5 @@ public:
         
         auto check = [](const int k, int bitCnt[limit]) {
             bitset<limit> bit2Val;
-            for (int i = 0; i < limit; i++) {
-                if (bitCnt[i] != 0) {
-                    bit2Val[i] = 1;
-                }
-            }
-            return bit2Val.to_ulong() >= k;
-        };
-        
-        for (int left = 0, right = 0; right < size; right++) {
-            if (nums[right] >= k) {
-                return 1;
-            }
 
-            update(nums[right], ADD);
-            while (check(k, bitCnt) == true) {
-                step = min(step, right - left + 1);
-                update(nums[left], DEL);
-                ++left;
-            }
-        }
-        return step != INT_MAX ? step : -1;
-    }
 
-};
