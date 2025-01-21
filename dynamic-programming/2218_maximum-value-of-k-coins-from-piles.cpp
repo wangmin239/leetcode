7571bc75264132cb85ae1bad5c8faab0a17a47cc
Min Wang <wangmin@phytium.com.cn>
2025-01-21 09:03:16 +0800

    [dynamic-programming]2218_maximum-value-of-k-coins-from-piles

---
+++ dynamic-programming/2218_maximum-value-of-k-coins-from-piles.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        vector<int> f(k + 1, -1);
+        f[0] = 0;
+        for (const auto& pile: piles) {
+            for (int i = k; i > 0; --i) {
+                int value = 0;
+                for (int t = 1; t <= pile.size(); ++t) {
+                    value += pile[t - 1];
+                    if (i >= t && f[i - t] != -1) {
+                        f[i] = max(f[i], f[i - t] + value);
+                    }
+                }
+            }
+        }
+        return f[k];
+    }
+};
+
