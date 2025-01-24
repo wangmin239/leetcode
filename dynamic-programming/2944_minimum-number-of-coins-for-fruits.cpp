aa0f3345d7000f95cb9b3220d4eaace9810d389e
Min Wang <wangmin@phytium.com.cn>
2025-01-24 10:31:21 +0800

    [dynamic-programming]2944_minimum-number-of-coins-for-fruits

---
+++ dynamic-programming/2944_minimum-number-of-coins-for-fruits.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minimumCoins(vector<int>& prices) {
+        unordered_map<int, int> memo;
+
+        function<int(int)> dp = [&](int index) -> int {
+            if (2 * index + 2 >= prices.size()) {
+                return prices[index];
+            }
+            if (!memo.count(index)) {
+                int minValue = INT_MAX;
+                for (int i = index + 1; i <= 2 * index + 2; i++) {
+                    minValue = min(minValue, dp(i));
+                }
+                memo[index] = prices[index] + minValue;
+            }
+            return memo[index];
+        };
+
+        return dp(0);
+    }
+};
+
+
+
+class Solution {
+public:
+    int minimumCoins(vector<int>& prices) {
+        int n = prices.size();
+        deque<pair<int, int>> queue;
+        queue.push_front({n, 0});
+        for (int i = n - 1; i >= 0; i--) {
+            while (!queue.empty() && queue.back().first >= 2 * i + 3) {
+                queue.pop_back();
+            }
+            int cur = queue.back().second + prices[i];
+            while (!queue.empty() && queue.front().second >= cur) {
+                queue.pop_front();
+            }
+            queue.push_front({i, cur});
+        }
+        return queue.front().second;
+    }
+};
