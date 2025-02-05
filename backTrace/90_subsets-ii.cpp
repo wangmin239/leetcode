45c7373516ea4f63ceb4ca2cd161ebcd144fa5dd
Min Wang <wangmin@phytium.com.cn>
2025-02-05 13:50:51 +0800

    [backTrace]90_subsets-ii

---
+++ backTrace/90_subsets-ii.cpp
@@ -0,0 +1,101 @@
+class Solution {
+public:
+    vector<int> t;
+    vector<vector<int>> ans;
+
+    vector<vector<int>> subsetsWithDup(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            t.clear();
+            bool flag = true;
+            for (int i = 0; i < n; ++i) {
+                if (mask & (1 << i)) {
+                    if (i > 0 && (mask >> (i - 1) & 1) == 0 && nums[i] == nums[i - 1]) {
+                        flag = false;
+                        break;
+                    }
+                    t.push_back(nums[i]);
+                }
+            }
+            if (flag) {
+                ans.push_back(t);
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<int> t;
+    vector<vector<int>> ans;
+
+    void dfs(bool choosePre, int cur, vector<int> &nums) {
+        if (cur == nums.size()) {
+            ans.push_back(t);
+            return;
+        }
+        dfs(false, cur + 1, nums);
+        if (!choosePre && cur > 0 && nums[cur - 1] == nums[cur]) {
+            return;
+        }
+        t.push_back(nums[cur]);
+        dfs(true, cur + 1, nums);
+        t.pop_back();
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        dfs(false, 0, nums);
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        auto backTrace = [&ans](vector<int>& nums, set<vector<int>>& bits,
+                         vector<int>& tmp, int len, int index, auto&& backTrace) {
+                if (tmp.size() == len) {
+                    if (bits.count(tmp) == 0) {
+                        ans.push_back(tmp);
+                        bits.insert(tmp);
+                    }
+                    
+                    return;
+                }
+                
+                const int offset = 10;
+                for (int i = index; i < nums.size(); i++) {
+                    tmp.push_back(nums[i]);
+                    backTrace(nums, bits, tmp, len, i + 1, backTrace);
+                    tmp.pop_back();
+                }
+                
+                return;
+            };
+            
+        ans.push_back({});
+        
+        sort(nums.begin(), nums.end());
+        for (int len = 1; len <= n; len++) {
+            set<vector<int>> bits;
+            vector<int> tmp;
+            backTrace(nums, bits, tmp, len, 0, backTrace);
+            
+        }
+        
+
+        
+        return ans;
+    }
+};
