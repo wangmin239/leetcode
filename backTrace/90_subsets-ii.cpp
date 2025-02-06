24a0b75e658178db1b548ae8d34384b099711473
Min Wang <wangmin@phytium.com.cn>
2025-02-06 09:51:01 +0800

    [backTrace]90_subsets-ii

--- backTrace/90_subsets-ii.cpp
+++ backTrace/90_subsets-ii.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> tmp;
+        sort(nums.begin(), nums.end(), less<int>());
+        backTrace(nums, 0, tmp, res);
+        return res;
+    }
+
+    void backTrace(vector<int> &nums, int startIndex, vector<int> &tmp, vector<vector<int>> &res) {
+        if (startIndex > nums.size()) {
+            return;
+        }
+
+        res.emplace_back(tmp);
+        for (int index = startIndex; index < nums.size(); index++) {
+            if (index > startIndex && nums[index] == nums[index - 1]) {
+                continue;
+            }
+            tmp.push_back(nums[index]);
+            backTrace(nums, index + 1, tmp, res);
+            tmp.pop_back();
+        }
+    }
+};
+
+
+class Solution {
+public:
     vector<int> t;
     vector<vector<int>> ans;
 
