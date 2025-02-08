1f0ab9335f07b6f29db519e77ce496587ce3ec49
Min Wang <wangmin@phytium.com.cn>
2025-02-08 09:37:33 +0800

    [dynamic-programming]2944_minimum-number-of-coins-for-fruits

--- dynamic-programming/2944_minimum-number-of-coins-for-fruits.cpp
+++ dynamic-programming/2944_minimum-number-of-coins-for-fruits.cpp
@@ -30,7 +30,7 @@ public:
         deque<pair<int, int>> queue;
         queue.push_front({n, 0});
         for (int i = n - 1; i >= 0; i--) {
-            while (!queue.empty() && queue.back().first >= 2 * i + 3) {
+            while (!queue.empty() && queue.back().first > 2 * i + 2) {
                 queue.pop_back();
             }
             int cur = queue.back().second + prices[i];
