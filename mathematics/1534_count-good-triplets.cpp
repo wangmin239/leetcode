19fc99fd12362394dced2a6679421dcc2b9647fd
Min Wang <wangmin@phytium.com.cn>
2025-04-14 08:51:40 +0800

    [mathematics]1534_count-good-triplets

---
+++ mathematics/1534_count-good-triplets.cpp
@@ -0,0 +1,70 @@
+/* Original Solution */
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int n = arr.size();
+        int cnt = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (abs(arr[i] - arr[j]) <= a &&
+                        abs(arr[j] - arr[k]) <= b &&
+                        abs(arr[i] - arr[k]) <= c) {
+                            ++cnt;
+                        }
+                }
+            }
+        }
+        
+        return cnt;
+    }
+};
+
+/* Official Solution 1 */
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int n = arr.size(), cnt = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    if (abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
+                        ++cnt;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+/* Official Solution 2 */
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int ans = 0, n = arr.size();
+        vector<int> sum(1001, 0);
+        for (int j = 0; j < n; ++j) {
+            for (int k = j + 1; k < n; ++k) {
+                if (abs(arr[j] - arr[k]) <= b) {
+                    int lj = arr[j] - a, rj = arr[j] + a;
+                    int lk = arr[k] - c, rk = arr[k] + c;
+                    int l = max(0, max(lj, lk)), r = min(1000, min(rj, rk));
+                    if (l <= r) {
+                        if (l == 0) {
+                            ans += sum[r];
+                        }
+                        else {
+                            ans += sum[r] - sum[l - 1];
+                        }
+                    }
+                }
+            }
+            for (int k = arr[j]; k <= 1000; ++k) {
+                ++sum[k];
+            }
+        }
+        return ans;
+    }
+};
