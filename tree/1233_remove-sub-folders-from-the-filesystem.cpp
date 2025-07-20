2ac95fbc7288ba1abb1e460b14d9bd5ecccf2f2f
Min Wang <wangmin@phytium.com.cn>
2025-07-20 20:16:12 +0800

    [tree]1233_remove-sub-folders-from-the-filesystem

---
+++ tree/1233_remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,94 @@
+
+/* Original Solution 1 */
+class Trie {
+public:
+    bool isEnd;
+    unordered_map<string, Trie*> path;
+    Trie() :isEnd(false) {
+    
+    }
+        
+};
+
+
+
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        Trie root;
+        
+        for (const auto& dir : folder) {
+            int len = dir.length();
+            Trie* node = &root;
+            int start = 1;
+            
+            /* split the dirent with the character '/' */
+            for (int end = start; end < len; end++) {
+                if (end == len - 1 || dir[end] == '/') {
+                    int dirLen = (end == len - 1) ? end - start + 1 : end - start;                     
+                    string subDir(dir.substr(start, dirLen));
+                    
+                    if (node->path.count(subDir) == 0) {
+                        node->path[subDir] = new Trie;
+                    }
+                    node = node->path[subDir];
+                    start = end + 1;
+                }
+            }
+            node->isEnd = true;
+        }
+        
+        vector<string> ans;
+        
+        auto dfs =[&ans](Trie* node, string&& parent, auto&& innerDfs) ->void {           
+            auto dirs = node->path;
+            
+            for (auto& [subDir, subNode]: dirs) {
+                if (subNode->isEnd == true) {
+                    ans.push_back(parent + "/" + subDir);
+                } else {
+                    innerDfs(subNode, parent + "/" + subDir, innerDfs);
+                }
+ 
+            }
+            return;
+        };
+        
+
+        dfs(&root,  "", dfs);
+        return ans;  
+    }
+};
+
+
+/* Official Solution 1 */
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+        vector<string> ans = {folder[0]};
+        for (int i = 1; i < folder.size(); ++i) {
+            if (int pre = ans.end()[-1].size(); !(pre < folder[i].size() && ans.end()[-1] == folder[i].substr(0, pre) && folder[i][pre] == '/')) {
+                ans.push_back(folder[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+/* Official Solution 2 */
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+        vector<string> ans = {folder[0]};
+        for (int i = 1; i < folder.size(); ++i) {
+            int pre = ans.back().size();
+            if (ans.back() == folder[i].substr(0, pre) && folder[i][pre] == '/') {
+                continue;
+            }
+            ans.push_back(folder[i]);
+        }
+        return ans;
+    }
+};
