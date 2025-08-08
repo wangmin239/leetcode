24aa943f244501b49e5bf8c2fd942c1669b27cda
Min Wang <wangmin@phytium.com.cn>
2025-08-08 15:12:48 +0800

    [dynamic-programming]808_soup-servings-ii

--- dynamic-programming/808_soup-servings-ii.cpp
+++ dynamic-programming/808_soup-servings-ii.cpp
@@ -51,8 +51,3 @@ public:
         return dp[n][n];
     }
 };
-
-作者：力扣官方题解
-链接：https://leetcode.cn/problems/soup-servings/solutions/1981704/fen-tang-by-leetcode-solution-0yxs/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
