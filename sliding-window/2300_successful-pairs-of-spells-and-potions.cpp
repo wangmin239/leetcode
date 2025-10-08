0786aa76b3cd16f88f085be8a07c94beb676d167
Min Wang <wangmin@phytium.com.cn>
2025-10-08 15:46:13 +0800

    [sliding-window]2300_successful-pairs-of-spells-and-potions

---
+++ sliding-window/2300_successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,58 @@
+/* Original Solution 1 */
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n = spells.size();
+        vector<int> ans(n);
+        
+        sort(potions.begin(), potions.end());
+        
+        for (int i = 0; i < n; i++) {
+            long long divisor = (success + spells[i] - 1) / spells[i];
+            auto iter = lower_bound(potions.begin(), potions.end(), divisor);
+            ans[i] = potions.end() - iter;
+        }
+        
+        return ans;
+    }
+};
+
+
+/* Official Solution 1 */
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        vector<int> res;
+        for (auto& i : spells) {
+            long long t = (success + i - 1) / i - 1;
+            res.push_back(potions.size() - (upper_bound(potions.begin(), potions.end(), t) - potions.begin()));
+        }
+        return res;
+    }
+};
+
+/* Official Solution 2 */
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        vector<int> res(spells.size());
+        vector<int> idx(spells.size());
+        iota(idx.begin(), idx.end(), 0);
+        sort(idx.begin(), idx.end(), [&](int a, int b) {
+            return spells[a] < spells[b];
+        });
+        sort(potions.begin(), potions.end(), [](int a, int b) {
+            return a > b;
+        });
+        for (int i = 0, j = 0; i < spells.size(); ++i) {
+            int p = idx[i];
+            int v = spells[p];
+            while (j < potions.size() && (long long) potions[j] * v >= success) {
+                ++j;
+            }
+            res[p] = j;
+        }
+        return res;
+    }
+};
