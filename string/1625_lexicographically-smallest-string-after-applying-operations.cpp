90a6b61d5ae752f6b18eccbad39aba580d87e8a9
Min Wang <wangmin@phytium.com.cn>
2025-10-19 21:20:06 +0800

    [1625]lexicographically-smallest-string-after-applying-operations

---
+++ string/1625_lexicographically-smallest-string-after-applying-operations.cpp
@@ -0,0 +1,64 @@
+/* Official Solution 1 */
+class Solution {
+public:
+    string findLexSmallestString(string s, int a, int b) {
+        int n = s.size();
+        vector<int> vis(n);
+        string res = s;
+        // 将 s 延长一倍，方便截取轮转后的字符串 t
+        s = s + s;
+        for (int i = 0; vis[i] == 0; i = (i + b) % n) {
+            vis[i] = 1;
+            for (int j = 0; j < 10; j++) {
+                int k_limit = b % 2 == 0 ? 0 : 9;
+                for (int k = 0; k <= k_limit; k++) {
+                    // 每次进行累加之前，重新截取 t
+                    string t = s.substr(i, n);
+                    for (int p = 1; p < n; p += 2) {
+                        t[p] = '0' + (t[p] - '0' + j * a) % 10;
+                    }
+                    for (int p = 0; p < n; p += 2) {
+                        t[p] = '0' + (t[p] - '0' + k * a) % 10;
+                    }
+                    res = min(res, t);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+/* Official Solution 2 */
+class Solution {
+public:
+    string findLexSmallestString(string s, int a, int b) {
+        int n = s.size();
+        string res = s;
+        s = s + s;
+        int g = gcd(b, n);
+        
+        auto add = [&](string& t, int start) {
+            int minVal = 10, times = 0;
+            for (int i = 0; i < 10; i++) {
+                int added = ((t[start] - '0') + i * a) % 10;
+                if (added < minVal) {
+                    minVal = added;
+                    times = i;
+                }
+            }
+            for (int i = start; i < n; i += 2) {
+                t[i] = '0' + ((t[i] - '0') + times * a) % 10;
+            }
+        };
+
+        for (int i = 0; i < n; i += g) {
+            string t = s.substr(i, n);
+            add(t, 1);
+            if (b % 2) {
+                add(t, 0);
+            }
+            res = min(res, t);
+        }
+        return res;
+    }
+};
