0e6de9260f150aa1b58edb36a491b77cf2f059dd
Min Wang <wangmin@phytium.com.cn>
2025-11-20 11:31:03 +0800

    [Greedy-algorithm]757_set-intersection-size-at-least-two

--- Greedy-algorithm/757_set-intersection-size-at-least-two.cpp
+++ Greedy-algorithm/757_set-intersection-size-at-least-two.cpp
@@ -1,3 +1,32 @@
+/* Official Solution  1 */
+class Solution {
+public:
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b){
+            if(a[0] == b[0]){
+                return a[1] > b[1];
+            }
+            return a[0] < b[0];
+        });  // 左边界升序，若相同，右边界降序
+        int res = 2, ls = intervals.back()[0], rs = ls + 1;
+        for(int i = intervals.size() - 2; i >= 0; i--){
+            if(intervals[i][1] >= rs){  // 有两个及以上的交点
+                continue;
+            }else if(intervals[i][1] < ls){  // 没有交点
+                ls = intervals[i][0];
+                rs = intervals[i][0] + 1;
+                res += 2;
+            }else{  // 一个交点
+                rs = ls;
+                ls = intervals[i][0];
+                res++;
+            }
+        }
+        return res;
+    }
+};
+
+/* Official Solution  2 */
 class Solution {
 public:
     void help(vector<vector<int>>& intervals, vector<vector<int>>& temp, int pos, int num) {
