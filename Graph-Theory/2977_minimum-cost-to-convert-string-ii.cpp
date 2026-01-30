ca1c08fe673799a66e5f9b9607198640fa6f68d0
Min Wang <wangmin@phytium.com.cn>
2026-01-30 09:25:32 +0800

    [Graph-Theory]2977_minimum-cost-to-convert-string-ii

---
+++ Graph-Theory/2977_minimum-cost-to-convert-string-ii.cpp
@@ -0,0 +1,168 @@
+/* Official Solution 1 */
+struct Trie {
+    Trie* child[26];
+    int id;
+
+    Trie() {
+        for (int i = 0; i < 26; ++i) {
+            child[i] = nullptr;
+        }
+        id = -1;
+    }
+};
+
+int add(Trie* node, const string& word, int& index) {
+    for (char ch: word) {
+        int i = ch - 'a';
+        if (!node->child[i]) {
+            node->child[i] = new Trie();
+        }
+        node = node->child[i];
+    }
+    if (node->id == -1) {
+        node->id = ++index;
+    }
+    return node->id;
+}
+
+void update(long long& x, long long y) {
+    if (x == -1 || y < x) {
+        x = y;
+    }
+}
+
+class Solution {
+public:
+    long long minimumCost(string source, string target, vector<string>& original, vector<string>& changed, vector<int>& cost) {
+        int n = source.size();
+        int m = original.size();
+        Trie* root = new Trie();
+
+        int p = -1;
+        vector<vector<int>> G(m * 2, vector<int>(m * 2, inf));
+        for (int i = 0; i < m * 2; ++i) {
+            G[i][i] = 0;
+        }
+        for (int i = 0; i < m; ++i) {
+            int x = add(root, original[i], p);
+            int y = add(root, changed[i], p);
+            G[x][y] = min(G[x][y], cost[i]);
+        }
+
+        for (int k = 0; k <= p; ++k) {
+            for (int i = 0; i <= p; ++i) {
+                if (G[i][k] == inf) {
+                    continue;
+                }
+                for (int j = 0; j <= p; ++j) {
+                    G[i][j] = min(G[i][j], G[i][k] + G[k][j]);
+                }
+            }
+        }
+
+        vector<long long> f(n, -1);
+        for (int j = 0; j < n; ++j) {
+            if (j > 0 && f[j - 1] == -1) {
+                continue;
+            }
+            long long base = (j == 0 ? 0 : f[j - 1]);
+            if (source[j] == target[j]) {
+                update(f[j], base);
+            }
+            Trie* u = root;
+            Trie* v = root;
+            for (int i = j; i < n; ++i) {
+                u = u->child[source[i] - 'a'];
+                v = v->child[target[i] - 'a'];
+                if (!u || !v) {
+                    break;
+                }
+                if (u->id != -1 && v->id != -1 && G[u->id][v->id] != inf) {
+                    update(f[i], base + G[u->id][v->id]);
+                }
+            }
+        }
+        return f[n - 1];
+    }
+
+private:
+    static constexpr int inf = INT_MAX / 2;
+};
+
+
+/* Official Solution 2 */
+struct Node {
+    Node* son[26]{};
+    int sid = -1; // 字符串的编号
+};
+
+class Solution {
+public:
+    long long minimumCost(string source, string target, vector<string>& original, vector<string>& changed, vector<int>& cost) {
+        Node* root = new Node();
+        int sid = 0;
+        auto put = [&](string& s) -> int {
+            Node* o = root;
+            for (char b: s) {
+                int i = b - 'a';
+                if (o->son[i] == nullptr) {
+                    o->son[i] = new Node();
+                }
+                o = o->son[i];
+            }
+            if (o->sid < 0) {
+                o->sid = sid++;
+            }
+            return o->sid;
+        };
+
+        // 初始化距离矩阵
+        int m = cost.size();
+        vector dis(m * 2, vector<int>(m * 2, INT_MAX / 2));
+        for (int i = 0; i < m * 2; i++) {
+            dis[i][i] = 0;
+        }
+        for (int i = 0; i < m; i++) {
+            int x = put(original[i]);
+            int y = put(changed[i]);
+            dis[x][y] = min(dis[x][y], cost[i]);
+        }
+
+        // Floyd 求任意两点最短路
+        for (int k = 0; k < sid; k++) {
+            for (int i = 0; i < sid; i++) {
+                if (dis[i][k] == INT_MAX / 2) { // 加上这句话，巨大优化！
+                    continue;
+                }
+                for (int j = 0; j < sid; j++) {
+                    dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
+                }
+            }
+        }
+
+        int n = source.size();
+        vector<long long> f(n + 1);
+        for (int i = n - 1; i >= 0; i--) {
+            // 不修改 source[i]
+            f[i] = source[i] == target[i] ? f[i + 1] : LONG_LONG_MAX / 2;
+            Node* p = root;
+            Node* q = root;
+            for (int j = i; j < n; j++) {
+                p = p->son[source[j] - 'a'];
+                q = q->son[target[j] - 'a'];
+                if (p == nullptr || q == nullptr) {
+                    break;
+                }
+                if (p->sid < 0 || q->sid < 0) {
+                    continue;
+                }
+                // 修改从 i 到 j 的这一段
+                int d = dis[p->sid][q->sid];
+                if (d < INT_MAX / 2) {
+                    f[i] = min(f[i], dis[p->sid][q->sid] + f[j + 1]);
+                }
+            }
+        }
+        return f[0] < LONG_LONG_MAX / 2 ? f[0] : -1;
+    }
+};
