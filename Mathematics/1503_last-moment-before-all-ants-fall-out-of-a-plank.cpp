21f4356f48364c882dcb60281cc61b4b394bc9bc
Min Wang <wangmin@phytium.com.cn>
2026-05-19 10:06:37 +0800

    [Mathematics]1503_last-moment-before-all-ants-fall-out-of-a-plank

---
+++ Mathematics/1503_last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -0,0 +1,17 @@
+/* Original Solution 1 */
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int time = 0;
+
+        for (int pos : left) {
+            time = max(time, pos);
+        }
+
+        for (int pos : right) {
+            time = max(time, n - pos);
+        }
+
+        return time;
+    }
+};
